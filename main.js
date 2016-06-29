fa0fec00e42bd327f0ea378d01bc0f96eba96fe5
Flatfact.com <trailersubthai@gmail.com>
2016-06-29 20:13:29 +0700

    Update main.js

--- main.js
+++ main.js
@@ -15,8 +15,11 @@ var day11 = day10+86400;
 var day12 = day11+86400;
 var day13 = day12+86400;
 var day14 = day13+86400;
+var avg_temp=0;
+var avg_humid=0;
+var avg_windspeed=0;
 var postcode = $('#postcode').val();
-var geocodingAPI = 'https://maps.googleapis.com/maps/api/geocode/json?address=ประเทศไทย'+ postcode +'&key=AIzaSyBQ9ygmgT3k-Jc6ZQujcFl1ls90oBu7F7s';
+var geocodingAPI = 'https://maps.googleapis.com/maps/api/geocode/json?address=ประเทศไทย'+ postcode;
 $("#loc").css("display", "block");
 $.getJSON(geocodingAPI, function (json) {
     var address = json.results[0].formatted_address;
@@ -37,6 +40,11 @@ $.getJSON(geocodingAPI, function (json) {
   success: function (data) {
       console.log("Day 1 : ",unix);
       console.log("Tempurature : ",data["daily"]["data"][0]["apparentTemperatureMin"]);
+      avg_temp=avg_temp+((data["daily"]["data"][0]["apparentTemperatureMin"])/14);
+      avg_windspeed=avg_windspeed+((data["daily"]["data"][0]["windSpeed"])/14);
+      avg_humid=avg_humid+((data["daily"]["data"][0]["humidity"])/14);
+      $('#humid1').text(data["daily"]["data"][0]["humidity"]);
+      $('#wind1').text(data["daily"]["data"][0]["windSpeed"]);
       $('#temp1').text(data["daily"]["data"][0]["apparentTemperatureMin"]);
       if(data["daily"]["data"][0]["icon"] == 'rain'){
         $('#icon1').html('<img src="weather/34.png" width="24px"> มีฝนตก');
@@ -70,6 +78,7 @@ $.getJSON(geocodingAPI, function (json) {
       }
   }
 });
+
    $.ajax({
   async:false,
   'global': false,
@@ -78,7 +87,12 @@ $.getJSON(geocodingAPI, function (json) {
   success: function (data) {
       console.log("Day 2 : ",day2);
       console.log("Tempurature : ",data["daily"]["data"][0]["apparentTemperatureMin"]);
-    $('#temp2').text(data["daily"]["data"][0]["apparentTemperatureMin"]);
+      avg_temp=avg_temp+((data["daily"]["data"][0]["apparentTemperatureMin"])/14);
+      avg_humid=avg_humid+((data["daily"]["data"][0]["humidity"])/14);
+      avg_windspeed=avg_windspeed+((data["daily"]["data"][0]["windSpeed"])/14);
+      $('#humid2').text(data["daily"]["data"][0]["humidity"]);
+      $('#wind2').text(data["daily"]["data"][0]["windSpeed"]);
+      $('#temp2').text(data["daily"]["data"][0]["apparentTemperatureMin"]);
     if(data["daily"]["data"][0]["icon"] == 'rain'){
       $('#icon2').html('<img src="weather/34.png" width="24px"> มีฝนตก');
     }
@@ -119,6 +133,11 @@ $.getJSON(geocodingAPI, function (json) {
   success: function (data) {
       console.log("Day 3 : ",day3);
       console.log("Tempurature : ",data["daily"]["data"][0]["apparentTemperatureMin"]);
+      avg_temp=avg_temp+((data["daily"]["data"][0]["apparentTemperatureMin"])/14);
+      avg_humid=avg_humid+((data["daily"]["data"][0]["humidity"])/14);
+      avg_windspeed=avg_windspeed+((data["daily"]["data"][0]["windSpeed"])/14);
+      $('#humid3').text(data["daily"]["data"][0]["humidity"]);
+      $('#wind3').text(data["daily"]["data"][0]["windSpeed"]);
       $('#temp3').text(data["daily"]["data"][0]["apparentTemperatureMin"]);
       if(data["daily"]["data"][0]["icon"] == 'rain'){
         $('#icon3').html('<img src="weather/34.png" width="24px"> มีฝนตก');
@@ -160,6 +179,11 @@ $.getJSON(geocodingAPI, function (json) {
   success: function (data) {
       console.log("Day 4 : ",day4);
       console.log("Tempurature : ",data["daily"]["data"][0]["apparentTemperatureMin"]);
+      avg_temp=avg_temp+((data["daily"]["data"][0]["apparentTemperatureMin"])/14);
+      avg_humid=avg_humid+((data["daily"]["data"][0]["humidity"])/14);
+      avg_windspeed=avg_windspeed+((data["daily"]["data"][0]["windSpeed"])/14);
+      $('#humid4').text(data["daily"]["data"][0]["humidity"]);
+      $('#wind4').text(data["daily"]["data"][0]["windSpeed"]);
       $('#temp4').text(data["daily"]["data"][0]["apparentTemperatureMin"]);
       if(data["daily"]["data"][0]["icon"] == 'rain'){
         $('#icon4').html('<img src="weather/34.png" width="24px"> มีฝนตก');
@@ -201,6 +225,11 @@ $.getJSON(geocodingAPI, function (json) {
   success: function (data) {
       console.log("Day 5 : ",day5);
       console.log("Tempurature : ",data["daily"]["data"][0]["apparentTemperatureMin"]);
+      avg_temp=avg_temp+((data["daily"]["data"][0]["apparentTemperatureMin"])/14);
+      avg_humid=avg_humid+((data["daily"]["data"][0]["humidity"])/14);
+      avg_windspeed=avg_windspeed+((data["daily"]["data"][0]["windSpeed"])/14);
+      $('#humid5').text(data["daily"]["data"][0]["humidity"]);
+      $('#wind5').text(data["daily"]["data"][0]["windSpeed"]);
       $('#temp5').text(data["daily"]["data"][0]["apparentTemperatureMin"]);
       if(data["daily"]["data"][0]["icon"] == 'rain'){
         $('#icon5').html('<img src="weather/34.png" width="24px"> มีฝนตก');
@@ -242,7 +271,12 @@ $.getJSON(geocodingAPI, function (json) {
   success: function (data) {
       console.log("Day 6 : ",day6);
       console.log("Tempurature : ",data["daily"]["data"][0]["apparentTemperatureMin"]);
+      avg_temp=avg_temp+((data["daily"]["data"][0]["apparentTemperatureMin"])/14);
+      $('#humid6').text(data["daily"]["data"][0]["humidity"]);
+      $('#wind6').text(data["daily"]["data"][0]["windSpeed"]);
       $('#temp6').text(data["daily"]["data"][0]["apparentTemperatureMin"]);
+      avg_humid=avg_humid+((data["daily"]["data"][0]["humidity"])/14);
+      avg_windspeed=avg_windspeed+((data["daily"]["data"][0]["windSpeed"])/14);
       if(data["daily"]["data"][0]["icon"] == 'rain'){
         $('#icon6').html('<img src="weather/34.png" width="24px"> มีฝนตก');
       }
@@ -272,35 +306,6 @@ $.getJSON(geocodingAPI, function (json) {
       }
       else{
         $('#icon6').text('NOPE')
-      } if(data["daily"]["data"][0]["icon"] == 'rain'){
-        $('#icon7').html('<img src="weather/34.png" width="24px"> มีฝนตก');
-      }
-      else if (data["daily"]["data"][0]["icon"] == 'clear-day') {
-        $('#icon7').html('<img src="weather/33.png" width="24px"> แจ่มใส');
-      }
-      else if (data["daily"]["data"][0]["icon"] == 'clear-night') {
-        $('#icon7').html('<img src="weather/33.png" width="24px"> แจ่มใส');
-      }
-      else if (data["daily"]["data"][0]["icon"] == 'sleet') {
-        $('#icon7').html('<img src="weather/36.png" width="24px"> มีฝนตกเล็กน้อย');
-      }
-      else if (data["daily"]["data"][0]["icon"] == 'wind') {
-        $('#icon7').html('<img src="weather/31.png" width="24px"> ลมแรง');
-      }
-      else if (data["daily"]["data"][0]["icon"] == 'fog') {
-        $('#icon7').html('<img src="weather/30.png" width="24px"> หมอกหนา');
-      }
-      else if (data["daily"]["data"][0]["icon"] == 'cloudy') {
-        $('#icon7').html('<img src="weather/37.png" width="24px"> มีเมฆมาก');
-      }
-      else if (data["daily"]["data"][0]["icon"] == 'partly-cloudy-day') {
-        $('#icon7').html('<img src="weather/37.png" width="24px"> มีเมฆบางส่วน');
-      }
-      else if (data["daily"]["data"][0]["icon"] == 'partly-cloudy-night') {
-        $('#icon7').html('<img src="weather/37.png" width="24px"> มีเมฆบางส่วน');
-      }
-      else{
-        $('#icon7').text('NOPE')
       }
   }
 });
@@ -312,7 +317,13 @@ $.getJSON(geocodingAPI, function (json) {
   success: function (data) {
       console.log("Day 7 : ",day7);
       console.log("Tempurature : ",data["daily"]["data"][0]["apparentTemperatureMin"]);
-      $('#temp7').text(data["daily"]["data"][0]["apparentTemperatureMin"]);
+      avg_temp=avg_temp+((data["daily"]["data"][0]["apparentTemperatureMin"])/14);
+      avg_humid=avg_humid+((data["daily"]["data"][0]["humidity"])/14);
+      avg_windspeed=avg_windspeed+((data["daily"]["data"][0]["windSpeed"])/14);
+      avg_windspeed=avg_windspeed+((data["daily"]["data"][0]["windSpeed"])/14);
+      $('#humid7').text(data["daily"]["data"][0]["humidity"]);
+      $('#wind7').text(data["daily"]["data"][0]["windSpeed"]);
+      $('#temp7').text(data["daily"]["data"][0]["apparentTemperatureMin"]);    
       if(data["daily"]["data"][0]["icon"] == 'rain'){
         $('#icon7').html('<img src="weather/34.png" width="24px"> มีฝนตก');
       }
@@ -353,7 +364,12 @@ $.getJSON(geocodingAPI, function (json) {
   success: function (data) {
       console.log("Day 8 : ",day8);
       console.log("Tempurature : ",data["daily"]["data"][0]["apparentTemperatureMin"]);
-      $('#temp8').text(data["daily"]["data"][0]["apparentTemperatureMin"]);
+      avg_temp=avg_temp+((data["daily"]["data"][0]["apparentTemperatureMin"])/14);
+      avg_humid=avg_humid+((data["daily"]["data"][0]["humidity"])/14);
+      avg_windspeed=avg_windspeed+((data["daily"]["data"][0]["windSpeed"])/14);
+      $('#humid8').text(data["daily"]["data"][0]["humidity"]);
+      $('#wind8').text(data["daily"]["data"][0]["windSpeed"]);
+      $('#temp8').text(data["daily"]["data"][0]["apparentTemperatureMin"]);      
       if(data["daily"]["data"][0]["icon"] == 'rain'){
         $('#icon8').html('<img src="weather/34.png" width="24px"> มีฝนตก');
       }
@@ -394,7 +410,12 @@ $.getJSON(geocodingAPI, function (json) {
   success: function (data) {
       console.log("Day 9 : ",day9);
       console.log("Tempurature : ",data["daily"]["data"][0]["apparentTemperatureMin"]);
-        $('#temp9').text(data["daily"]["data"][0]["apparentTemperatureMin"]);
+      avg_temp=avg_temp+((data["daily"]["data"][0]["apparentTemperatureMin"])/14);
+      $('#humid9').text(data["daily"]["data"][0]["humidity"]);
+      $('#wind9').text(data["daily"]["data"][0]["windSpeed"]);
+      $('#temp9').text(data["daily"]["data"][0]["apparentTemperatureMin"]);        
+        avg_humid=avg_humid+((data["daily"]["data"][0]["humidity"])/14);
+        avg_windspeed=avg_windspeed+((data["daily"]["data"][0]["windSpeed"])/14);
       if(data["daily"]["data"][0]["icon"] == 'rain'){
         $('#icon9').html('<img src="weather/34.png" width="24px"> มีฝนตก');
       }
@@ -435,7 +456,12 @@ $.getJSON(geocodingAPI, function (json) {
   success: function (data) {
       console.log("Day 10 : ",day10);
       console.log("Tempurature : ",data["daily"]["data"][0]["apparentTemperatureMin"]);
-        $('#temp10').text(data["daily"]["data"][0]["apparentTemperatureMin"]);
+      avg_temp=avg_temp+((data["daily"]["data"][0]["apparentTemperatureMin"])/14);
+      avg_humid=avg_humid+((data["daily"]["data"][0]["humidity"])/14);
+      avg_windspeed=avg_windspeed+((data["daily"]["data"][0]["windSpeed"])/14);
+      $('#humid10').text(data["daily"]["data"][0]["humidity"]);
+      $('#wind10').text(data["daily"]["data"][0]["windSpeed"]);
+      $('#temp10').text(data["daily"]["data"][0]["apparentTemperatureMin"]);
       if(data["daily"]["data"][0]["icon"] == 'rain'){
         $('#icon10').html('<img src="weather/34.png" width="24px"> มีฝนตก');
       }
@@ -476,7 +502,12 @@ $.getJSON(geocodingAPI, function (json) {
   success: function (data) {
       console.log("Day 11 : ",day11);
       console.log("Tempurature : ",data["daily"]["data"][0]["apparentTemperatureMin"]);
-        $('#temp11').text(data["daily"]["data"][0]["apparentTemperatureMin"]);
+      avg_temp=avg_temp+((data["daily"]["data"][0]["apparentTemperatureMin"])/14);
+      avg_humid=avg_humid+((data["daily"]["data"][0]["humidity"])/14);
+      avg_windspeed=avg_windspeed+((data["daily"]["data"][0]["windSpeed"])/14);
+      $('#humid11').text(data["daily"]["data"][0]["humidity"]);
+      $('#wind11').text(data["daily"]["data"][0]["windSpeed"]);
+      $('#temp11').text(data["daily"]["data"][0]["apparentTemperatureMin"]);      
       if(data["daily"]["data"][0]["icon"] == 'rain'){
         $('#icon11').html('<img src="weather/34.png" width="24px"> มีฝนตก');
       }
@@ -517,7 +548,12 @@ $.getJSON(geocodingAPI, function (json) {
   success: function (data) {
       console.log("Day 12 : ",day12);
       console.log("Tempurature : ",data["daily"]["data"][0]["apparentTemperatureMin"]);
-        $('#temp12').text(data["daily"]["data"][0]["apparentTemperatureMin"]);
+      avg_humid=avg_humid+((data["daily"]["data"][0]["humidity"])/14);
+      avg_temp=avg_temp+((data["daily"]["data"][0]["apparentTemperatureMin"])/14);
+      avg_windspeed=avg_windspeed+((data["daily"]["data"][0]["windSpeed"])/14);
+      $('#humid12').text(data["daily"]["data"][0]["humidity"]);
+      $('#wind12').text(data["daily"]["data"][0]["windSpeed"]);
+      $('#temp12').text(data["daily"]["data"][0]["apparentTemperatureMin"]);
       if(data["daily"]["data"][0]["icon"] == 'rain'){
         $('#icon12').html('<img src="weather/34.png" width="24px"> มีฝนตก');
       }
@@ -558,7 +594,12 @@ $.getJSON(geocodingAPI, function (json) {
   success: function (data) {
       console.log("Day 13 : ",day13);
       console.log("Tempurature : ",data["daily"]["data"][0]["apparentTemperatureMin"]);
-        $('#temp13').text(data["daily"]["data"][0]["apparentTemperatureMin"]);
+      avg_temp=avg_temp+((data["daily"]["data"][0]["apparentTemperatureMin"])/14);
+      avg_humid=avg_humid+((data["daily"]["data"][0]["humidity"])/14);
+      avg_windspeed=avg_windspeed+((data["daily"]["data"][0]["windSpeed"])/14);
+      $('#humid13').text(data["daily"]["data"][0]["humidity"]);
+      $('#wind13').text(data["daily"]["data"][0]["windSpeed"]);
+      $('#temp13').text(data["daily"]["data"][0]["apparentTemperatureMin"]);
       if(data["daily"]["data"][0]["icon"] == 'rain'){
         $('#icon13').html('<img src="weather/34.png" width="24px"> มีฝนตก');
       }
@@ -599,7 +640,12 @@ $.getJSON(geocodingAPI, function (json) {
   success: function (data) {
       console.log("Day 14 : ",day14);
       console.log("Tempurature : ",data["daily"]["data"][0]["apparentTemperatureMin"]);
-        $('#temp14').text(data["daily"]["data"][0]["apparentTemperatureMin"]);
+      avg_humid=avg_humid+((data["daily"]["data"][0]["humidity"])/14);
+      avg_temp=avg_temp+((data["daily"]["data"][0]["apparentTemperatureMin"])/14);
+      avg_windspeed=avg_windspeed+((data["daily"]["data"][0]["windSpeed"])/14);
+      $('#humid14').text(data["daily"]["data"][0]["humidity"]);
+      $('#wind14').text(data["daily"]["data"][0]["windSpeed"]);
+      $('#temp14').text(data["daily"]["data"][0]["apparentTemperatureMin"]);      
       if(data["daily"]["data"][0]["icon"] == 'rain'){
         $('#icon14').html('<img src="weather/34.png" width="24px"> มีฝนตก');
       }
@@ -630,7 +676,33 @@ $.getJSON(geocodingAPI, function (json) {
       else{
         $('#icon14').text('NOPE')
       }
+
   }
+
 });
 }); //get JSON function
+
+$("#calculate").click(function(){
+
+  $("#resultcard").css("display","block");
+      var nook_constant = 6.877;
+      var algho1 = (math.eval('(((5336/'+avg_temp+'^2))e^(21.07-(5336/'+avg_temp+')))*1343*('+nook_constant+')'));
+      var algho2 = (math.eval('(0.000665*6.43(1-(-0.536*'+avg_windspeed+'))(1-'+avg_humid+')(5.624))'));
+      var algho3 = (math.eval('0.0437*(1000/18)*(((5336/'+avg_temp+'^2))e^(21.07-(5336/'+avg_temp+')))+0.665*10^(-3)'));
+      var algho4 = (math.eval((algho1+algho1+algho2)/2));
+      var result = (math.eval(algho4/algho3));
+      var waterlevel = (math.eval($('#waterlevel').val())*0.6);
+      if($('#waterlevel').val() < 1){
+        $("#output").text('Error');
+      }
+      else{
+      var output = (math.eval(waterlevel/(result)));    
+      console.log("avg_temp =",avg_temp);
+      console.log("avg_humid =",avg_humid)
+      console.log("avg_windspeed =",avg_windspeed);
+      console.log(math.eval(output+'*20'));          
+      $("#output").text('เวลาที่ใช้ '+parseInt(math.eval(output+'*20'))+' วัน');
+      }
+});//calculate onclick
+
 });//onclick function
